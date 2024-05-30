d47b4beeb806a83ae0694a58fe4b70d0659b1e63
ibnebattuta <ibnebattuta@duck.com>
2024-05-30 23:19:28 +0530

    Getting Additional Resources

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -3,14 +3,19 @@ using Entities.Models;
 
 namespace Repository;
 
-internal sealed class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+internal sealed class CompanyRepository : RepositoryBase<Company>,ICompanyRepository
 {
-    public CompanyRepository(RepositoryContext repositoryContext)
-        : base(repositoryContext)
-    { }
+	public CompanyRepository(RepositoryContext repositoryContext)
+		: base(repositoryContext)
+	{
+	}
 
     public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
         FindAll(trackChanges)
             .OrderBy(c => c.Name)
             .ToList();
+
+    public Company GetCompany(Guid companyId, bool trackChanges) =>
+        FindByCondition(c => c.Id.Equals(companyId), trackChanges)
+        .SingleOrDefault();
 }
