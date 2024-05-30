2a3b227ddb31de99d64838e26c4c252830ee6288
ibnebattuta <ibnebattuta@duck.com>
2024-05-30 23:18:26 +0530

    Handling GET Requests

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -3,10 +3,14 @@ using Entities.Models;
 
 namespace Repository;
 
-internal sealed class CompanyRepository : RepositoryBase<Company>,ICompanyRepository
+internal sealed class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
 {
-	public CompanyRepository(RepositoryContext repositoryContext)
-		: base(repositoryContext)
-	{
-	}
+    public CompanyRepository(RepositoryContext repositoryContext)
+        : base(repositoryContext)
+    { }
+
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+        FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
 }
